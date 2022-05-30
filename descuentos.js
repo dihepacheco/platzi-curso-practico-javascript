be1d2fa645c47ae2674a5b42d59ad3fde25b3380
dihepacheco <dihe94@gmail.com>
2022-05-30 08:48:47 -0500

    Agregue archivos de figuras  descuentos y actualización archivo de figuras

---
+++ descuentos.js
@@ -0,0 +1,32 @@
+//const precioOriginal = 100;
+// const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento / 100);
+
+    return precioConDescuento;
+}
+
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const DiscountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, DiscountValue);
+
+    const resultPrice = document.getElementById("resultPrice");
+    resultPrice.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
+
+/* console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento,
+}); */
+
+
--- figuras.js
+++ figuras.js
@@ -1,1 +1,49 @@
-        console.log("Hello")
+// #Abres
+// console.group("nombre que desees");
+// #Cierras
+// console.groupEnd();
+
+//Código cuadrado
+
+/* Cuando dentro de las funciones recibimos - le llamamos parametros */
+
+/* Cuando llamamos a una función lo que enviamos se llaman argumentos */
+
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+perimetroCuadrado()
+
+function areaCuadrado(lado) {
+    return lado * lado;
+}
+
+areaCuadrado()
+
+//Código triangulo
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+}
+
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
+
+// Código Circulo
+
+function diametroCirculo(radio) {
+    return radio * 2;
+}
+
+const PI = Math.PI;
+
+function perimetroCirculo(radio){
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+}
+
+function areaCirculo(radio) {
+    return (radio * radio) * PI;
+}
